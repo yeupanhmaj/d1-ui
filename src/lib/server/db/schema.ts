161751da1c1a8d1721157fa5142135bf12280e4c
Hoang Nguyen <wallker.night@gmail.com>
2026-06-08 15:49:46 +0700

    add simple ui

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,5 +2,6 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
-	name: text('name').notNull()
+	name: text('name').notNull(),
+	age: integer('age')
 });
